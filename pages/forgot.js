81ec757daaf4c28393eb46133aed99432ff510de
Akash Singh <akashs@datetheramp.com>
2020-07-02 14:58:49 +0530

    Eslint Most  Rule is Generic ,

--- pages/forgot.js
+++ pages/forgot.js
@@ -1,26 +1,26 @@
-import React, { Fragment } from 'react';
-import Head from 'next/head';
-import brand from '../static/text/brand';
-import ForgotPassword from '../components/Forms/ForgotPassword';
+import React, { Fragment } from "react";
+import Head from "next/head";
+import brand from "../static/text/brand";
+import ForgotPassword from "../components/Forms/ForgotPassword";
 
 function Login() {
-  return (
-    <Fragment>
-      <Head>
-        <title>
-          { brand.starter.name }
-          &nbsp; - Retrieve Your Password
-        </title>
-      </Head>
-      <div>
-        <ForgotPassword />
-      </div>
-    </Fragment>
-  );
+	return (
+		<Fragment>
+			<Head>
+				<title>
+					{brand.starter.name}
+					&nbsp; - Retrieve Your Password
+				</title>
+			</Head>
+			<div>
+				<ForgotPassword />
+			</div>
+		</Fragment>
+	);
 }
 
 ForgotPassword.getInitialProps = async () => ({
-  namespacesRequired: ['common', 'hosting-landing'],
+	namespacesRequired: ["common", "hosting-landing"],
 });
 
 export default Login;
